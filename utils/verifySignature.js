db6049577775d76c4b90630615fef43d739f25da
palnativenkatesh <166120319+palnativenkatesh@users.noreply.github.com>
2025-06-09 15:44:01 +0530

    verifySignature.js

--- utils/verifySignature.js
+++ utils/verifySignature.js
@@ -1,1 +1,7 @@
+import { ethers } from 'ethers';
+
+export async function verifySignature(address, signature, message) {
+  const recoveredAddress = ethers.utils.verifyMessage(message, signature);
+  return recoveredAddress.toLowerCase() === address.toLowerCase();
+}
 
